55f66b6b4683891994632dbdc0ee03230e8a7d6f
SHIVAM PATHAK <shivam6386pathak@gmail.com>
2022-09-12 23:59:35 +0530

    Now the code has been connected with telegram bot

--- t_bot01.py
+++ t_bot01.py
@@ -6,12 +6,13 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+print("Initializing the Bot.....")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
+    application = ApplicationBuilder().token('5599948853:AAH3DCmj0DFYPS4dAedYLQxyVJHt-Sy8O8w').build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
